4eb8b929d2f200806d83b0824ce1a263b5964905
KlarseQ <klarse4ever@gmail.com>
2023-11-06 20:19:34 +0300

    add appsettings; register repository and db context in di

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UserService.DBContext;
+using UserService.Interfaces;
+using UserService.Repositories;
+
+namespace UserService;
+
+internal static class Program
 {
     public static void Main(string[] args)
     {
-        
+        var host = CreateHostBuilder(args).Build();
+        InitDataBase(host);
+        host.Run();
+    }
+    
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                var configuration = hostContext.Configuration;
+                var connectionString = configuration.GetValue<string>("AppConnection");
+
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+                services.AddScoped<IUserRepository, UserRepository>();
+            });
+
+    private static void InitDataBase(IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        var yourService = services.GetRequiredService<AppDbContext>();
+        yourService.Database.Migrate();
     }
 }
