5e9c91ba2acf49e1b97c6de35673472c419a040e
KlarseQ <klarse4ever@gmail.com>
2023-11-06 20:16:24 +0300

    add user repository

---
+++ UserService/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using UserService.Models;
+
+namespace UserService.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAndDomainAsync(Guid userId, string domain);
+
+    IQueryable<User> GetAll();
+}
---
+++ UserService/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.DBContext;
+using UserService.Interfaces;
+using UserService.Models;
+
+namespace UserService.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByIdAndDomainAsync(Guid userId, string domain)
+    {
+        return await _context.Users.Include(u=>u.Tags).AsNoTracking()
+            .FirstOrDefaultAsync(f => f.UserId == userId && f.Domain == domain);
+    }
+
+    public IQueryable<User> GetAll() =>
+        _context.Users.Include(u => u.Tags).AsNoTracking().AsQueryable();
+}
